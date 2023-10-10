ed80b104de84cb377ff5f858e2072fe7e9fd68a9
r4venz <zexceed12300@gmail.com>
2023-10-10 08:50:20 +0700

    Constant: exportDataToExternalFile function

--- app/src/main/java/com/zexceed/restaurant/util/Constants.kt
+++ app/src/main/java/com/zexceed/restaurant/util/Constants.kt
@@ -1,9 +1,31 @@
 package com.zexceed.restaurant.util
 
+import android.content.ContentValues
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.annotation.RequiresApi
+
 object Constants {
 
     const val TAG = "Response::::::"
 
     const val API_BASE_URL = "http://192.168.100.47:5000/Api/"
 
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun exportDataToExternalFile(context: Context, filename: String, data: ByteArray, mimeType: String) {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+            put(MediaStore.MediaColumns.MIME_TYPE, mimeType)
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
+        }
+        val resolver = context.contentResolver
+        val uri: Uri? = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
+        if (uri != null) {
+            val outputStream = resolver.openOutputStream(uri)
+            outputStream?.write(data)
+        }
+    }
 }
