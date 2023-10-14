13bdb8bc5a0b7daf5cc159f5c97a51b41cf0899e
r4venz <zexceed12300@gmail.com>
2023-10-14 10:26:32 +0700

    AdminActivity: Add Table Dialog

--- app/src/main/java/com/zexceed/restaurant/AdminActivity.kt
+++ app/src/main/java/com/zexceed/restaurant/AdminActivity.kt
@@ -1,12 +1,18 @@
 package com.zexceed.restaurant
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.dialog.MaterialDialogs
 import com.zexceed.restaurant.adapter.TableAdapter
 import com.zexceed.restaurant.apiservices.ApiServices
 import com.zexceed.restaurant.databinding.ActivityAdminBinding
+import com.zexceed.restaurant.databinding.DialogAddTableBinding
 import com.zexceed.restaurant.util.Constants.TAG
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -24,8 +30,6 @@ class AdminActivity : AppCompatActivity() {
         binding = ActivityAdminBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Log.d(TAG, "onCreate: testttttt Admin ACtivity")
-
         binding.apply {
             val coroutineScope = CoroutineScope(Dispatchers.IO)
             coroutineScope.launch {
@@ -41,6 +45,25 @@ class AdminActivity : AppCompatActivity() {
                     }
                 }
             }
+
+            btnAddTable.setOnClickListener {
+                showAddTableDialog()
+            }
         }
+    }
+
+    private fun showAddTableDialog() {
+        val dialogBinding = DialogAddTableBinding.inflate(layoutInflater)
+        val dialogBuilder = MaterialAlertDialogBuilder(this@AdminActivity)
+        dialogBuilder.setView(dialogBinding.root)
+        val customDialog = dialogBuilder.create()
+
+        dialogBinding.apply {
+            btnCancel.setOnClickListener {
+                customDialog.dismiss()
+            }
+        }
+
+        customDialog.show()
     }
 }
